0ac8b70aa7e7581d2b9ddd667b246c52b7161c95
nyarla <thotep@nyarla.net>
2009-10-07 18:17:03 +0900

    config.example.pl: update.

--- config.example.pl
+++ config.example.pl
@@ -7,18 +7,49 @@ my $basedir = '/path/to/script/dir';
 
 return {
 
-    flavour => {
+    flavour     => {
         dir     => "${basedir}/flavours",
         default => 'html',
     },
 
-    plugin  => {
+    # cache       => {
+    #     class   => 'Ccahe::FileCache',
+    #     args    => {
+    #         cache_root => "${basedir}/cache",
+    #     },
+    #     deref   => 0,
+    # },
+
+    plugin      => {
         plugin_dir          => [ "${basedir}/plugins" ],
         plugin_state_dir    => "${basedir}/states",
     },
 
-    plugins => {
+    plugins     => {
         # { plugin => 'PluginName', config => \%config },
     }
+
+    vars        => {
+    # foo => 'bar',
+    },
+
+    dispatch    => {
+        regexp => {
+            # foo => qr{regexp},
+        },
+        rule    => [
+            # {
+            #     path        => '/{year}/{month}/{day}',
+            #     flavour     => {
+            #         'hlosxom::flavour property' => 'static value',
+            #     },
+            #     condition   => {
+            #         method      => 'GET' || 'POST' || '...etc',
+            #         function    => sub { return 1 || 0 };
+            #     },
+            #     after_hook  => sub { my ( $res, $flavour ) = @_; }
+            # },
+        ],
+    },
 
 };
